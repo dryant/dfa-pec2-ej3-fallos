74749a15832e3cdeeaf0c958849bda23d06b66d1
dryant <dryant@MacBook-Pro-de-dryant.local>
2023-11-27 11:13:40 +0100

    Add: Creates Modules
    - Se han creado todos los modulos necesarios con la estructura recomendada

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,28 @@
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { CategoriesListComponent } from './Components/categories/categories-list/categories-list.component';
-import { CategoryFormComponent } from './Components/categories/category-form/category-form.component';
-import { FooterComponent } from './Components/footer/footer.component';
-import { HeaderComponent } from './Components/header/header.component';
-import { HomeComponent } from './Components/home/home.component';
-import { LoginComponent } from './Components/login/login.component';
-import { PostFormComponent } from './Components/posts/post-form/post-form.component';
-import { PostsListComponent } from './Components/posts/posts-list/posts-list.component';
-import { ProfileComponent } from './Components/profile/profile.component';
-import { RegisterComponent } from './Components/register/register.component';
-import { AuthInterceptorService } from './Services/auth-interceptor.service';
-import { FormatDatePipe } from './Pipes/format-date.pipe';
-import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { BrowserModule } from "@angular/platform-browser";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { CategoriesListComponent } from "./Components/categories/categories-list/categories-list.component";
+import { CategoryFormComponent } from "./Components/categories/category-form/category-form.component";
+import { FooterComponent } from "./Components/footer/footer.component";
+import { HeaderComponent } from "./Components/header/header.component";
+import { HomeComponent } from "./Components/home/home.component";
+import { LoginComponent } from "./Components/login/login.component";
+import { PostFormComponent } from "./Components/posts/post-form/post-form.component";
+import { PostsListComponent } from "./Components/posts/posts-list/posts-list.component";
+import { ProfileComponent } from "./Components/profile/profile.component";
+import { RegisterComponent } from "./Components/register/register.component";
+import { AuthInterceptorService } from "./Services/auth-interceptor.service";
+import { FormatDatePipe } from "./Pipes/format-date.pipe";
+import { DashboardComponent } from "./Components/dashboard/dashboard.component";
+
+import { AuthModule } from "./blog/auth/auth.module";
+import { CategoryModule } from "./blog/category/category.module";
+import { PostModule } from "./blog/post/post.module";
+import { UserModule } from "./blog/user/user.module";
+import { SharedModule } from "./blog/shared/shared.module";
 
 @NgModule({
   declarations: [
@@ -39,6 +45,11 @@ import { DashboardComponent } from './Components/dashboard/dashboard.component';
     AppRoutingModule,
     ReactiveFormsModule,
     HttpClientModule,
+    AuthModule,
+    CategoryModule,
+    PostModule,
+    UserModule,
+    SharedModule,
   ],
   providers: [
     {
